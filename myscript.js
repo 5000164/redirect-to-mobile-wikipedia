8f9a243d09ec607ee885c8c6792ea9b935b44f2b
SUGAWARA Hiroshi <sncts35326@gmail.com>
2015-03-23 00:30:34 +0900

    content_scripts でページの表示時に location.href でリダイレクトしていた処理をやめ、 background でページを表示する前にアクセス先 URL を変更する

--- myscript.js
+++
@@ -1,6 +0,0 @@
-var url = location.href;
-if (url.indexOf("://ja.wikipedia.org/") != -1) {
-    var redirectUrl = url.replace("://ja.wikipedia.org/", "://ja.m.wikipedia.org/");
-    location.href = redirectUrl;
-}
-//# sourceMappingURL=myscript.js.map
--- myscript.ts
+++
@@ -1,6 +0,0 @@
-var url:string = location.href;
-
-if (url.indexOf("://ja.wikipedia.org/") != -1) {
-    var redirectUrl:string = url.replace("://ja.wikipedia.org/", "://ja.m.wikipedia.org/");
-    location.href = redirectUrl;
-}
